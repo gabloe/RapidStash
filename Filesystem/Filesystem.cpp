3ede3ea3397279738b69e749ee9b07935afd7ce0
Gabriel Loewen <gloewen@crimson.ua.edu>
2015-06-25 23:37:11 -0500

    Working on the filesystem

--- Filesystem/Filesystem.cpp
+++ Filesystem/Filesystem.cpp
@@ -3,33 +3,62 @@
 STORAGE::Filesystem::Filesystem(const char* fname) : file(fname) {
 	// Set up file directory if the backing file is new.
 	if (file.isNew()) {
-		dir = new FileDirectory(0);
+		dir = new FileDirectory();
 		writeFileDirectory(dir);
 	} else {
 		dir = readFileDirectory();
 	}
 }
 
+STORAGE::File *STORAGE::Filesystem::createNewFile(std::string fname) {
+	size_t nameLen = fname.size();
+	if (nameLen > File::MAXNAMELEN) {
+		nameLen = File::MAXNAMELEN;
+	}
+
+	// Construct the file object.
+	File *f = new File();
+	memcpy(f->name, fname.c_str(), nameLen);
+	f->name[nameLen] = '\0';
+
+	// Modify the file directory
+	f->location = dir->insert(fname);
+
+	// The size allocated to new files is constant
+	f->virtualSize = STORAGE::FileDirectory::MINALLOCATION;
+
+	// Raw write a placeholder byte.  The actual file contents will be written later.
+	file.raw_write(FilePlaceholder, sizeof(FilePlaceholder), f->location);
+
+#ifdef DEBUGGING
+	// Some testing to make sure we are writing the correct stuff
+	char *test = file.raw_read(f->location, sizeof(FilePlaceholder));
+	for (int i = 0; i < sizeof(FilePlaceholder); ++i) {
+		if (test[i] != FilePlaceholder[i]) {
+			logEvent(ERROR, "Error verifying file placeholder");
+			shutdown(FAILURE);
+		}
+	}
+#endif
+
+	return f;
+}
+
 void STORAGE::Filesystem::shutdown(int code) {
+	writeFileDirectory(dir);
 	file.shutdown(code);
 }
 
 void STORAGE::Filesystem::writeFileDirectory(FileDirectory *fd) {
+	logEvent(EVENT, "Writing file directory");
 	file.raw_write(reinterpret_cast<char*>(fd), FileDirectory::SIZE, 0);
 }
 
 STORAGE::FileDirectory *STORAGE::Filesystem::readFileDirectory() {
+	logEvent(EVENT, "Reading file directory");
 	char *buffer = file.raw_read(0, FileDirectory::SIZE);
-	FileDirectory *directory = new FileDirectory(0);
+	FileDirectory *directory = new FileDirectory();
 	memcpy(directory, reinterpret_cast<FileDirectory*>(buffer), FileDirectory::SIZE);
-	for (int i = 0; i < MAXFILES; ++i) {
-		if (directory->positions[i] != -1) {
-			std::ostringstream d;
-			d << directory->positions[i];
-			logEvent(ERROR, "Directory read failed, read " + d.str() + ", expected -1.");
-			shutdown(FAILURE);
-		}
-	}
 	free(buffer);
 	return directory;
 }
--- Filesystem/Filesystem.h
+++ Filesystem/Filesystem.h
@@ -4,7 +4,6 @@
 
 #include "DynamicMemoryMappedFile.h"
 #include "Logging.h"
-
 #include <limits>
 
 /*
@@ -34,14 +33,67 @@ Filesystem structure:
 namespace STORAGE {
 	static const unsigned short MAXFILES = std::numeric_limits<unsigned short>::max();
 
+	// Data to initially write to file location.  Used to reclaim files that get created but never written.
+	static const char FilePlaceholder[] = { 0xd,0xe,0xa,0xd,0xb,0xe,0xe,0xf };
+
+	struct File {
+		static const unsigned int MAXNAMELEN = 32;
+		char name[MAXNAMELEN];  // Up to 32 bytes for filenames (256-bit)
+		size_t size;
+		size_t virtualSize;
+		off_t location;
+		char *data;
+		File() : size(0), virtualSize(0), location(0) {}
+		File(char *rawBuffer, off_t loc) : location(loc) {
+			memcpy(name, rawBuffer, MAXNAMELEN);
+			memcpy(&size, rawBuffer + MAXNAMELEN, sizeof(size_t));
+			memcpy(&virtualSize, rawBuffer + MAXNAMELEN + sizeof(size_t), sizeof(size_t));
+			memcpy(data, rawBuffer + MAXNAMELEN + sizeof(size_t) * 2, size);
+		}
+		size_t getRawSize() {
+			return sizeof(size_t) * 2 + MAXNAMELEN + size;
+		}
+		char *getRawBuffer() {
+			char *buffer = (char*)malloc(getRawSize());
+			memcpy(buffer, name, MAXNAMELEN);
+			memcpy(buffer + MAXNAMELEN, reinterpret_cast<char*>(&size), sizeof(size_t));
+			memcpy(buffer + MAXNAMELEN + sizeof(size_t), reinterpret_cast<char*>(&virtualSize), sizeof(size_t));
+			memcpy(buffer + MAXNAMELEN + sizeof(size_t) * 2, data, size);
+			return buffer;
+		}
+	};
+
+	struct FileMeta {
+		static const unsigned int SIZE = File::MAXNAMELEN + 2 * sizeof(size_t);
+		size_t nameSize;
+		char name[File::MAXNAMELEN];
+		size_t position;
+	};
+
 	struct FileDirectory {
-		static const unsigned int SIZE = sizeof(unsigned short) + sizeof(off_t) * MAXFILES;
+		static const size_t MINALLOCATION = 512;  // Pre-Allocate 512 bytes per file.
+		static const unsigned int SIZE	=	2 * sizeof(unsigned short) + 
+											sizeof(size_t) + 
+											FileMeta::SIZE * MAXFILES;
 		unsigned short numFiles;
-		off_t positions[MAXFILES];
-		FileDirectory(unsigned short num) : numFiles(num) {
+		unsigned short firstFree;
+		size_t nextRawSpot;
+		FileMeta files[MAXFILES];
+		FileDirectory() : numFiles(0), firstFree(0), nextRawSpot(SIZE) {
 			for (int i = 0; i < MAXFILES; ++i) {
-				positions[i] = -1;
+				memset(&files[i], 0, FileMeta::SIZE);
 			}
+		}
+		size_t insert(std::string name, unsigned int size = MINALLOCATION) {
+			unsigned short spot = firstFree;
+			size_t location = nextRawSpot;
+			nextRawSpot += size;
+			firstFree++;
+			size_t nameSize = name.size();
+			memcpy(&files[spot].nameSize, &nameSize, sizeof(size_t));
+			memcpy(files[spot].name, name.c_str(), name.size());
+			memcpy(&files[spot].position, &location, sizeof(size_t));
+			return location;
 		}
 	};
 
@@ -49,6 +101,7 @@ namespace STORAGE {
 	public:
 		Filesystem(const char* fname);
 		void shutdown(int code = SUCCESS);
+		File *createNewFile(std::string);
 
 	private:
 		DynamicMemoryMappedFile file;
