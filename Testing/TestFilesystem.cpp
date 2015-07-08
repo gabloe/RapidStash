9db4e58b3fa5f23ca41c3b28da5e53c3a9d4f7e9
deathly809 <deathly809@gmail.com>
2015-07-07 22:12:22 -0500

    Better test?

--- Testing/TestFilesystem.cpp
+++ Testing/TestFilesystem.cpp
@@ -1,8 +1,17 @@
 
-#include <vector>
 
 #include "Filesystem.h"
 #include "Logging.h"
+
+
+#define VECTOR 0
+
+#if VECTOR
+#include <vector>
+#else
+#include <array>
+#endif
+
 
 void foo(STORAGE::Filesystem *f, std::string name) {
 	STORAGE::File file = f->select(name);
@@ -22,27 +31,37 @@ void foo(STORAGE::Filesystem *f, std::string name) {
 }
 
 int main() {
+	const int NumThreads = 50;
 	static int c;
 	using namespace std::literals;
 	STORAGE::Filesystem f("test.stash");
-
+#if VECTOR
   std::vector<std::thread> threads;
+#else
+	std::array<std::thread,NumThreads> threads;
+#endif
 	for (int j = 0; j < 50000; ++j) {
 
 		srand((unsigned int)time(NULL));
-		for (int i = 0; i < 50; ++i) {
+		for (int i = 0; i < NumThreads; ++i) {
 			std::ostringstream os;
 			os << "MyFile" << rand();
+#if VECTOR
 			threads.push_back(std::thread(foo, &f, os.str()));
+#else
+			threads[i] = std::thread(foo, &f, os.str());
+#endif
 		}
-		
+
 		for (auto& th : threads) {
 			th.join();
 		}
-
-    threads.clear();
-
-		std::cout << j << std::endl;
+#if VECTOR
+		threads.clear();
+#endif
+		if (j % 100 == 0) {
+			std::cout << j << std::endl;
+		}
 	}
 	f.shutdown();
 	return 0;
