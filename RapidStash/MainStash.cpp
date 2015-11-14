51bf9d9674cc60046804915a0877119494d8e23b
Jeffrey Robinson <deathly809@gmail.com>
2015-11-13 23:16:03 -0600

    tests

--- RapidStash/MainStash.cpp
+++ RapidStash/MainStash.cpp
@@ -1,18 +1,19 @@
 
+#include <vector>
 #include <iostream>
 #include <string>
 #include <gason.h>
 
 #include <chrono>
 
-void performance(char* msg) {
+void performance(std::string msg) {
 	const int iterations = 100000;
 	char herp[1024];
 	char* endptr = nullptr;
 	std::cout << "Parsing: " << msg << std::endl;
 	auto start = std::chrono::high_resolution_clock::now();
 	for (int i = 0; i < iterations; ++i) {
-		strcpy_s(herp, msg);
+		strcpy_s(herp, msg.c_str());
 		JsonAllocator allocator;
 		JsonValue value;
 
@@ -27,8 +28,22 @@ void performance(char* msg) {
  }
 
 int main(int argc, char* argv[]) {
-	if (argc != 2) return 0;
-	performance(argv[1]);
+	
+	std::vector<std::string> tests;
+
+	tests.push_back("{\"a\" : 1,}");
+	tests.push_back("{\"a\" : 1,\"b\" : 1}");
+	tests.push_back("{\"a\" : 1,\"b\" : 1,\"c\" : 1}");
+	tests.push_back("{\"a\" : 1,\"b\" : 1,\"c\" : 1,\"d\" : 1}");
+	tests.push_back("{\"a\" : 1,\"b\" : 1,\"c\" : 1,\"d\" : 1,\"e\" : 1}");
+	tests.push_back("{\"a\" : 1,\"b\" : 1,\"c\" : 1,\"d\" : 1,\"e\" : 1,\"f\" : 1}");
+	tests.push_back("{\"a\" : 1,\"b\" : 1,\"c\" : 1,\"d\" : 1,\"e\" : 1,\"f\" : 1,\"g\" : 1}");
+	tests.push_back("{\"a\" : 1,\"b\" : 1,\"c\" : 1,\"d\" : 1,\"e\" : 1,\"f\" : 1, \"g\" : 1, \"h\" : 1}");
+	tests.push_back("{\"a\" : 1,\"b\" : 1,\"c\" : 1,\"d\" : 1,\"e\" : 1,\"f\" : 1, \"g\" : 1, \"h\" : 1, \"i\" : 1}");
+
+	for (int i = 0; i < tests.size(); ++i) {
+		performance(tests[i]);
+	}
 	std::cin.get();
 	return 0;
 }
