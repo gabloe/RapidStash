4eb47e68a14e8f540ef833721dff853a6c2e19ef
Jeffrey Robinson <deathly809@gmail.com>
2015-11-13 22:49:18 -0600

    herp

--- RapidStash/MainStash.cpp
+++ RapidStash/MainStash.cpp
@@ -14,5 +14,10 @@ int main(int argc, char* argv[]) {
 	if (status != JSON_OK) {
 		std::cout << "Failure?" << std::endl;
 	}
+	JsonIterator beg = begin(value);
+	while (beg != end(value)) {
+		std::cout << beg->key << ":" << beg->value.toString() << std::endl;
+		++beg;
+	}
 	return 0;
 }
