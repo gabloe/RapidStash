ac79d08e118bb87d6fe0e1af6825fc3ad695686f
Jeffrey Robinson <deathly809@gmail.com>
2015-11-13 23:04:37 -0600

    Added performance

--- RapidStash/MainStash.cpp
+++ RapidStash/MainStash.cpp
@@ -1,23 +1,34 @@
 
 #include <iostream>
-
+#include <string>
 #include <gason.h>
 
+#include <chrono>
+
+void performance(char* msg) {
+	const int iterations = 100000;
+	char herp[1024];
+	char* endptr = nullptr;
+	std::cout << "Parsing: " << herp << std::endl;
+	auto start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < iterations; ++i) {
+		strcpy_s(herp, msg);
+		JsonAllocator allocator;
+		JsonValue value;
+
+		int status = jsonParse(herp, &endptr, &value, allocator);
+		if (status != JSON_OK) {
+			std::cout << "Failure?" << std::endl;
+		}
+	}
+	auto end = std::chrono::high_resolution_clock::now();
+
+	std::cout << iterations  / (0.000001 + std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count())<< " iterations per millisecond" << std::endl;
+ }
 
 int main(int argc, char* argv[]) {
 	if (argc != 2) return 0;
-	char *herp = argv[1];
-	JsonValue value;
-	JsonAllocator allocator;
-	char* endptr = nullptr;
-	int status = jsonParse(herp, &endptr, &value, allocator);
-	if (status != JSON_OK) {
-		std::cout << "Failure?" << std::endl;
-	}
-	JsonIterator beg = begin(value);
-	while (beg != end(value)) {
-		std::cout << beg->key << ":" << beg->value.toNumber() << std::endl;
-		++beg;
-	}
+	performance(argv[1]);
+	std::cin.get();
 	return 0;
 }
