2dc0a93813cd45ac9dc63c747b996fd7bd318edb
Jeffrey Robinson <deathly809@gmail.com>
2015-11-14 00:24:39 -0600

    FileSystemPerformance

--- RapidStash/MainStash.cpp
+++ RapidStash/MainStash.cpp
@@ -34,6 +34,35 @@ void performance(std::string& msg) {
 	std::cout << iterations  / (0.000001 + std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count())<< " iterations per millisecond" << std::endl;
  }
 
+void fileSystemPerformance(STORAGE::Filesystem* fs, std::string filename, std::string should) {
+	const int iterations = 100000;
+	char* herp = new char[1024 * 1024];
+	char* endptr = nullptr;
+
+	JsonAllocator allocator;
+
+
+	std::cout << "Parsing: " << should << std::endl;
+	auto start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < iterations; ++i) {
+		File file = fs->select(filename);
+		STORAGE::IO::Reader reader = fs->getReader(file);
+		std::string msg = reader.readString();
+		memcpy(herp, msg.c_str(), msg.length() + 1);
+		JsonValue value;
+
+		int status = jsonParse(herp, &endptr, &value, allocator);
+		if (status != JSON_OK) {
+			std::cout << "Failure?" << std::endl;
+		}
+		allocator.deallocate();
+	}
+	delete herp;
+	auto end = std::chrono::high_resolution_clock::now();
+
+	std::cout << iterations / (0.000001 + std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()) << " iterations per millisecond" << std::endl;
+}
+
 int main(int argc, char* argv[]) {
 	
 	std::vector<std::string> tests;
@@ -108,7 +137,7 @@ int main(int argc, char* argv[]) {
 	tests.push_back(ss.str());
 
 	STORAGE::Filesystem fs("derp.txt");
-	
+
 	for (int i = 0; i < tests.size(); ++i) {
 		std::string* ref = &(tests[i]);
 
@@ -120,13 +149,15 @@ int main(int argc, char* argv[]) {
 			STORAGE::IO::Writer writer = fs.getWriter(file);
 			writer.write(ref->c_str(), ref->length() + 1);
 		}
-		else {
-			file = fs.select(fName);
-			std::cout << "Exists" << std::endl;
-		}
-		STORAGE::IO::Reader reader = fs.getReader(file);
-		performance(reader.readString());
 	}
+
+	for (int i = 0; i < tests.size(); ++i) {
+		std::string fName = "test" + toString(i);
+		std::string ref = (tests[i]);
+		fileSystemPerformance(&fs, fName, ref);
+	}
+
+
 
 	fs.shutdown();
 	std::cin.get();
