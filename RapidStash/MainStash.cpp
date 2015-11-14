d515956d4c57bdde9a93ebf953871f487800922f
Jeffrey Robinson <deathly809@gmail.com>
2015-11-13 23:46:33 -0600

    tests

--- RapidStash/MainStash.cpp
+++ RapidStash/MainStash.cpp
@@ -1,4 +1,5 @@
 
+#include <sstream>
 #include <vector>
 #include <iostream>
 #include <string>
@@ -6,18 +7,17 @@
 
 #include <chrono>
 
-void performance(std::string msg) {
+void performance(std::string* msg) {
 	const int iterations = 100000;
-	char herp[1024];
+	char* herp = new char[1024*1024];
 	char* endptr = nullptr;
 
 	JsonAllocator allocator;
 
-
-	std::cout << "Parsing: " << msg << std::endl;
+	std::cout << "Parsing: " << *msg << std::endl;
 	auto start = std::chrono::high_resolution_clock::now();
 	for (int i = 0; i < iterations; ++i) {
-		strcpy_s(herp, msg.c_str());
+		memcpy(herp, msg->c_str(), msg->length() + 1);
 		JsonValue value;
 
 		int status = jsonParse(herp, &endptr, &value, allocator);
@@ -25,6 +25,7 @@ void performance(std::string msg) {
 			std::cout << "Failure?" << std::endl;
 		}
 	}
+	delete herp;
 	auto end = std::chrono::high_resolution_clock::now();
 
 	std::cout << iterations  / (0.000001 + std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count())<< " iterations per millisecond" << std::endl;
@@ -34,7 +35,7 @@ int main(int argc, char* argv[]) {
 	
 	std::vector<std::string> tests;
 
-	// Linear JSON
+	// Linear JSON of numbers
 	tests.push_back("{\"a\" : 1}");
 	tests.push_back("{\"a\" : 1,\"b\" : 1}");
 	tests.push_back("{\"a\" : 1,\"b\" : 1,\"c\" : 1}");
@@ -44,6 +45,18 @@ int main(int argc, char* argv[]) {
 	tests.push_back("{\"a\" : 1,\"b\" : 1,\"c\" : 1,\"d\" : 1,\"e\" : 1,\"f\" : 1,\"g\" : 1}");
 	tests.push_back("{\"a\" : 1,\"b\" : 1,\"c\" : 1,\"d\" : 1,\"e\" : 1,\"f\" : 1, \"g\" : 1, \"h\" : 1}");
 	tests.push_back("{\"a\" : 1,\"b\" : 1,\"c\" : 1,\"d\" : 1,\"e\" : 1,\"f\" : 1, \"g\" : 1, \"h\" : 1, \"i\" : 1}");
+
+	// Linear JSON of strings
+	tests.push_back("{\"a\" : \"1\"}");
+	tests.push_back("{\"a\" : \"1\", \"b\" : \"1\"}");
+	tests.push_back("{\"a\" : 1,\"b\" : \"1\",\"c\" : \"1\"}");
+	tests.push_back("{\"a\" : 1,\"b\" : \"1\",\"c\" : \"1\",\"d\" : \"1\"}");
+	tests.push_back("{\"a\" : 1,\"b\" : \"1\",\"c\" : \"1\",\"d\" : \"1\",\"e\" : \"1\"}");
+	tests.push_back("{\"a\" : 1,\"b\" : \"1\",\"c\" : \"1\",\"d\" : \"1\",\"e\" : \"1\",\"f\" : \"1\"}");
+	tests.push_back("{\"a\" : 1,\"b\" : \"1\",\"c\" : \"1\",\"d\" : \"1\",\"e\" : \"1\",\"f\" : \"1\",\"g\" : \"1\"}");
+	tests.push_back("{\"a\" : 1,\"b\" : \"1\",\"c\" : \"1\",\"d\" : \"1\",\"e\" : \"1\",\"f\" : \"1\", \"g\" : \"1\", \"h\" : \"1\"}");
+	tests.push_back("{\"a\" : 1,\"b\" : \"1\",\"c\" : \"1\",\"d\" : \"1\",\"e\" : \"1\",\"f\" : \"1\", \"g\" : \"1\", \"h\" : \"1\", \"i\" : \"1\"}");
+
 
 	// Recursive JSON objects
 	tests.push_back("{ \"a\" : { \"b\" : 5} }");
@@ -78,9 +91,22 @@ int main(int argc, char* argv[]) {
 	tests.push_back(" \"a\" : [ [1], [2], [3], [4] ]");
 	tests.push_back(" \"a\" : [ [1], [2], [3], [4] , [5] ]");
 	tests.push_back(" \"a\" : [ [1], [2], [3], [4] , [5] , [6] ]");
+	
+	std::stringstream ss;
+	
+	const int TerribleLength = 200;
+
+	ss << "{";
+	for (int i = 1; i < TerribleLength; ++i) {
+		ss << "\"a" << i << "\":" << i << ",";
+	}
+	ss << "\"a1000\":" << TerribleLength << "}";
+
+	tests.push_back(ss.str());
 
 	for (int i = 0; i < tests.size(); ++i) {
-		performance(tests[i]);
+		std::string* ref = &(tests[i]);
+		performance(ref);
 	}
 	std::cin.get();
 	return 0;
